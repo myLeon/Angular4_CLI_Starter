f702f8eb65dea00099d4e99fcc0e040be6f96470
李林 <leonhoney@163.com>
2017-04-16 10:51:13 +0800

    基于angular4,coreui 初始化

---
+++ src/app/components/modals.component.ts
@@ -0,0 +1,7 @@
+import { Component, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal/modal.component';
+
+@Component({
+  templateUrl: 'modals.component.html'
+})
+export class ModalsComponent { }
